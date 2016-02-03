747f122d418c7de7997398a6dc3e9a2a13cb2fa7
Niklas Nihlén <niklas.nihlen@gmail.com>
2016-02-03 10:02:23 +0100

    More sleep :(

--- start.sh
+++ start.sh
@@ -44,6 +44,7 @@ sleep ${VARNISH_D_DELAY:=10}
 echo "starting varnishd"
 varnishd -f /etc/varnish/default.vcl -s malloc,100M -a 0.0.0.0:${VARNISH_PORT} &
 pid="$!"
+sleep 5
 
 if [ ${VARNISH_LOG:=0} -eq 1 ]; then
   echo "Starting log to console"
